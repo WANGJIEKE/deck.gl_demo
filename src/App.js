b9d3062a3d037b5319c4a489489e4345ddecd489
Tongjie <tjwang0221@gmail.com>
2020-02-18 16:19:48 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import DeckGL from '@deck.gl/react'
+import { LineLayer } from '@deck.gl/layers'
+import './App.css'
 
 function App() {
+  const viewState = {
+    longitude: -122.41669,
+    latitude: 37.7853,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+  }
+
+  const data = [
+    { sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }
+  ]
+
+  const layers = [
+    new LineLayer({ id: 'line-layer', data })
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DeckGL viewState={viewState} layers={layers} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
